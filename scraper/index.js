84351e9e81999d4dec3142a2c5648af3f1dd6e83
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-08-24 13:59:04 +0700

    fix?

--- scraper/index.js
+++ scraper/index.js
@@ -1,10 +1,18 @@
 let path = require('path')
 let fs = require('fs')
+const assert = require('assert')
 
 ; (async () => { 
   let base = path.join(__dirname, './')
   let dir = await fs.readdirSync(base)
-  for (let file of dir.filter(v => v.endsWith('.js'))) {
-    require('./' + file)
+  for (let file of dir.filter(v => v.endsWith('.js') && v !== path.basename(__filename))) {
+    console.error('Run', file)
+    try {
+      require('./' + file)
+      assert.ok(file)
+      console.log('Done run', file)
+    } catch (e) {
+      assert.ok(e.length < 1, file + '\n\n' + e)
+    }
   }
 })()
