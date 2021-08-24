895f08d374f59e15c1c7738a58d82aefc29fe9f6
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-08-24 13:40:14 +0700

    .

--- scraper/index.js
+++ scraper/index.js
@@ -4,8 +4,7 @@ let fs = require('fs')
 ; (async () => { 
   let base = path.join(__dirname, './')
   let dir = await fs.readdirSync(base)
-  dir.map(v => base + v)
   for (let file of dir.filter(v => v.endsWith('.js'))) {
-    await (await require(file))
+    await (await require('./' + file))
   }
 })()
