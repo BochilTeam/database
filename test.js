6368db5a5a21c9e3ac1d8ccecdf411f97a21c9ef
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-08-29 16:54:09 +0700

    update test.js

--- test.js
+++ test.js
@@ -1,46 +1,53 @@
 const fs = require('fs')
 const path = require('path')
 const assert = require('assert')
+const { spawn } = require('child_process')
 
 async function check() {
-    let base = path.join(__dirname, './')
-    let dir = await readdir(base)
-    dir.map(v => base + v)
-    let files = []
-    let dirnames = dir
-    while (dirnames.length !== 0) {
-        if (await isDirectory(dirnames[0])) {
-            let path = await readdir(dirnames[0])
-            path.filter(v => !v.endsWith('node_modules') && !v.startsWith('.')).map(file => dirnames.push(dirnames[0] + '/' + file))
-        } else if (dirnames[0].endsWith('.json')) {
-            files.push(dirnames[0])
-        }
-        dirnames.shift()
+  let base = path.join(__dirname, '../')
+  let dir = await readdir(base)
+  let files = []
+  let dirnames = dir.filter(gx).map(v => base + v)
+  while (dirnames.length !== 0) {
+    if (await isDirectory(dirnames[0])) {
+      let path = await readdir(dirnames[0])
+      path.filter(v => !v.endsWith('node_modules') && !v.startsWith('.')).map(file => dirnames.push(dirnames[0] + '/' + file))
+    } else if (dirnames[0].endsWith('.json') || dirnames[0].endsWith('.js')) {
+      files.push(dirnames[0])
     }
-    
-    for (let file of files) {
-        console.error('Checking', file)
-        try {
-            await parse(file)
-            assert.ok(file)
-            console.log('Done checking', file)
-        }  catch (e) {
-            assert.ok(e.length < 1, file + '\n\n' + e)
-        }
+    dirnames.shift()
+  }
+
+  for (let file of files) {
+    if (file === require.resolve(__filename)) continue
+    console.error('Checking', file)
+    try {
+      if (file.endsWith('.json')) parse(file)
+      else if (file.endsWith('.js')) spawn(process.argv0, ['-c', file])
+      assert.ok(file)
+      console.log('Done checking', file)
+    } catch (e) {
+      assert.ok(e.length < 1, file + '\n\n' + e)
+      process.exit(1)
     }
-    // console.log(process.argv0)
+  }
+  // console.log(process.argv0)
 }
 
 async function readdir(path) {
-    return await fs.readdirSync(path)
+  return await fs.readdirSync(path)
 }
 
 async function isDirectory(path) {
-    return await fs.statSync(path).isDirectory()
+  return await fs.statSync(path).isDirectory()
 }
 
 async function parse(path) {
-    return JSON.parse(await fs.readFileSync(path))
+  return JSON.parse(await fs.readFileSync(path))
+}
+
+function gx(file) {
+    return !file.endsWith('node_modules') && !file.startsWith('.')
 }
 
 check()
