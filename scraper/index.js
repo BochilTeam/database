5aedb619b78912fae481cf9e5a713c5bb4381085
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-08-30 13:48:44 +0700

    .

--- scraper/index.js
+++ scraper/index.js
@@ -1,19 +1,28 @@
 let path = require('path')
 let fs = require('fs')
 const assert = require('assert')
+const { spawn } = require('child_process')
 
 ; (async () => { 
   let base = path.join(__dirname, './')
   let dir = await fs.readdirSync(base)
-  for (let file of dir.filter(v => v.endsWith('.js') && v !== path.basename(__filename))) {
+  for (let file of dir.filter(v => v.endsWith('.js') && v !== path.basename(__filename)).map(v => base + v)) {
     console.error('Run', file)
     try {
-      await require('./' + file)
+      node(file)
       assert.ok(file)
       console.log('Done run', file)
     } catch (e) {
-      assert.ok(e.length < 1, file)
-      console.error(e)
+      assert.ok(e.length < 1, file + '\n\n' + e)
+      process.exit(1)
     }
   }
 })()
+
+function node(file) {
+  return new Promise((resolve, reject) => {
+    spawn(process.argv0, ['-c', file])
+      .on('close', resolve)
+      .stderr.on('data', reject)
+  })
+}
