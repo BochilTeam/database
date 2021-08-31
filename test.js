39195b2b56d84170ff79cd1c393a6b9c52042eaa
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-08-31 09:46:20 +0700

    Update test.js

--- test.js
+++ test.js
@@ -51,7 +51,7 @@ function node(file) {
     spawn(process.argv0, ['-c', file])
       .on('close', resolve)
       .stderr.on('data', err => {
-      return reject(err.toString())
+      throw reject(err.toString())
     })
   })
 }
