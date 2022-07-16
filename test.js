19ec1ed90114c7ecf3b684189112e0af07b3e7ea
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-07-16 12:36:44 +0700

    Throw error if parse json failed and fix syntax

--- test.js
+++ test.js
@@ -4,43 +4,48 @@ const assert = require('assert')
 
 async function check() {
     let base = path.join(__dirname, './')
-    let dir = await readdir(base)
+    let dir = readdir(base)
     dir.map(v => base + v)
     let files = []
     let dirnames = dir
     while (dirnames.length !== 0) {
-        if (await isDirectory(dirnames[0])) {
-            let path = await readdir(dirnames[0])
-            path.filter(v => !v.endsWith('node_modules') && !v.startsWith('.')).map(file => dirnames.push(dirnames[0] + '/' + file))
+        if (isDirectory(dirnames[0]) && filter(dirnames[0])) {
+            let path = readdir(dirnames[0])
+            path.filter(filter).map(file => dirnames.push(dirnames[0] + '/' + file))
         } else if (dirnames[0].endsWith('.json')) {
             files.push(dirnames[0])
         }
         dirnames.shift()
     }
-    
+
     for (let file of files) {
         console.error('Checking', file)
         try {
-            await parse(file)
+            parse(file)
             assert.ok(file)
             console.log('Done checking', file)
-        }  catch (e) {
-            assert.ok(e.length < 1, file + '\n\n' + e)
+        } catch (e) {
+            assert.ok(e, file + '\n\n' + e)
+            if (e) process.exit(1)
         }
     }
     // console.log(process.argv0)
 }
 
-async function readdir(path) {
-    return await fs.readdirSync(path)
+function readdir(path) {
+    return fs.readdirSync(path)
 }
 
-async function isDirectory(path) {
-    return await fs.statSync(path).isDirectory()
+function isDirectory(path) {
+    return fs.statSync(path).isDirectory()
 }
 
-async function parse(path) {
-    return JSON.parse(await fs.readFileSync(path))
+function parse(path) {
+    return JSON.parse(fs.readFileSync(path))
+}
+
+function filter(fileName) {
+    return !fileName.startsWith('node_modules') && !fileName.startsWith('.')
 }
 
 check()
